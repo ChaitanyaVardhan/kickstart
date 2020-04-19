5d339c1b12cade4c9cab59faa3207803a1ef9809
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2020-04-19 07:07:58 +0530

    bike tour v1 fixes

--- 2020/round-b/bike_tour.py
+++ 2020/round-b/bike_tour.py
@@ -1,10 +1,10 @@
-def solve(N, S):
-    i = 0
+def solve_for_peaks(N, S):
+    i = 1
     peak_count = 0
-    while i < len(S)
+    while i < len(S):
         if i < len(S) - 1 and S[i] > S[i+1] and S[i] > S[i-1]:
             peak_count += 1
-        if i = len(S) - 1 and S[i] > S[i-1]:
+        if i == len(S) - 1 and S[i] > S[i-1]:
             peak_count += 1
         i += 1
 
@@ -13,8 +13,8 @@ def solve(N, S):
 
 if __name__ == "__main__":
     T = int(input())
-    for i in range T:
+    for i in range(T):
         N = int(input())
-        S = [int(i) for i in input.split()]
+        S = [int(i) for i in input().split()]
         ans = solve_for_peaks(N, S)
-        print("Case #{}: {}".format(str(i), ans))
+        print("Case #{}: {}".format(str(i+1), ans))
